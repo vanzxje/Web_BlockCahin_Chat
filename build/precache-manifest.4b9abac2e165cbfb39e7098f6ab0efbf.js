2a181565c5089fb75e99acc9eb9c18826a043e81
vanzxje <vanle200275@gmail.com>
2025-04-05 16:30:58 +0700

    Blockchain

---
+++ build/precache-manifest.4b9abac2e165cbfb39e7098f6ab0efbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c322c711156e64cbcaa0",
+    "url": "/static/css/main.288786de.chunk.css"
+  },
+  {
+    "revision": "c322c711156e64cbcaa0",
+    "url": "/static/js/main.c322c711.chunk.js"
+  },
+  {
+    "revision": "3b1b34177b89fc08658d",
+    "url": "/static/css/1.af0dd601.chunk.css"
+  },
+  {
+    "revision": "3b1b34177b89fc08658d",
+    "url": "/static/js/1.3b1b3417.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "11152e05c63e6e324657f9fa1f72c0a0",
+    "url": "/static/media/arrow.11152e05.png"
+  },
+  {
+    "revision": "6c16e47ac2249b62821dbb75fdfa442f",
+    "url": "/index.html"
+  }
+];
